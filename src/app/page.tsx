3e11676f39771bd387a967e2c80aa651cd393b1c
Marcelo <marceloengecomp@gmail.com>
2023-11-06 19:23:41 -0300

    Banner Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div className='relative h-full lg:h-[140vh]'>
       <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'></div>
+          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+            <Image
+              src='/banner.png'
+              alt='The Witcher'
+              fill={true}
+              className='h-[65vh] object-cover object-top lg:h[95-h]'
+            />
+          </div>
 
           <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
             The Witcher
@@ -20,9 +27,15 @@ export default function Home() {
           </p>
         </div>
 
-        <button className='w-24 rounded-full bg-white px-4 py-2 font-bold text-black md:w-32 lg:w-40'>
-          Play
-        </button>
+        <div className='flex space-x-3'>
+          <button className='md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md-py-2.5'>
+            Play
+          </button>
+
+          <button className='md:text-xl flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md-py-2.5'>
+            More Info
+          </button>
+        </div>
       </main>
     </div>
   );
