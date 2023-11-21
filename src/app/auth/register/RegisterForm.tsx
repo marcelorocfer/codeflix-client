e3d3ac8bad940548c943e56d43ba909c2775fba8
Marcelo <marceloengecomp@gmail.com>
2023-11-21 16:32:06 -0300

    Login Form

--- src/app/auth/register/RegisterForm.tsx
+++ src/app/auth/register/RegisterForm.tsx
@@ -1,55 +1,13 @@
 'use client';
 
+import { AuthForm } from '@/app/components/AuthForm';
+import { InputField } from '@/app/components/InputField';
 import React from 'react';
-import { InputField } from '../InputField';
 
 export default function RegisterForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     alert('submit from register');
     e.preventDefault();
   };
-  return (
-    <form
-      onSubmit={handleSubmit}
-      className='flex w-full max-w-md flex-col space-y-4 rounded bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg'
-    >
-      <div className='flex flex-col items-center space-y-4'>
-        <h1 className='text-3xl font-bold'>Register</h1>
-        <p className='text-sm text-gray-500'>
-          Already have an account?{' '}
-          <a href='#' className='text-red-500 hover:underline'>
-            Login
-          </a>
-        </p>
-      </div>
-      <div className='mt-8 flex flex-col space-y-4'>
-        <InputField
-          id='email'
-          type='email'
-          label='Email'
-          placeholder='Enter your email'
-        />
-        <InputField
-          id='password'
-          type='password'
-          label='Password'
-          placeholder='Enter your password'
-        />
-        <InputField
-          id='confirmPassword'
-          type='password'
-          label='Confirm Password'
-          placeholder='Confirm your password'
-        />
-      </div>
-      <div className='flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0'>
-        <button
-          type='submit'
-          className='flex w-full items-center justify-center rounded-lg bg-red-500 hover:bg-red-600 px-4 py-2 text-sm font-semibold text-white sm:w-auto sm:px-8'
-        >
-          Register
-        </button>
-      </div>
-    </form>
-  );
+  return <AuthForm formType='register' onSubmit={handleSubmit} />;
 }
--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { InputField } from "./InputField";
+
 type AuthFormProps = {
   formType: 'login' | 'register';
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -9,5 +11,62 @@ export const AuthForm: React.FC<AuthFormProps> = ({
   formType,
   onSubmit,
 }) => {
-  return <div>AuthForm</div>;
+  return (
+    <form 
+      onSubmit={onSubmit}
+      className='flex w-full max-w-md flex-col space-y-4 rounded bg-[#141414] bg-opacity-90 px-4 py-8 shadow-lg'
+    >
+      <div className='flex flex-col items-center space-y-4'>
+        <h1
+          className='text-3xl font-bold'
+        >
+          {formType === 'login' ? 'Login' : 'Register'}
+        </h1>
+        <p>
+          {formType === 'login' 
+            ? 'New to app?' 
+            : 'Already have an account?'}{' '}
+        </p>
+        <a 
+          href={formType === 'login' ? '/auth/register' : '/auth/login'}
+          className='text-red-500 hover:underline'
+        >
+          {formType === 'login' ? 'Register' : 'Login'}
+        </a>
+      </div>
+      <div className='mt-8 flex flex-col space-y-4'>
+        <InputField
+          id='email'
+          type='email'
+          label='Email'
+          placeholder='Enter your email' 
+        />
+
+        <InputField
+          id='password'
+          type='password'
+          label='Password'
+          placeholder='Enter your password' 
+        />
+
+        {formType === 'register' && (
+          <InputField
+            id='confirmPassword'
+            type='password'
+            label='Confirm Password'
+            placeholder='Confirm your password'
+          />
+        )}
+      </div>
+
+      <div className='flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0'>
+        <button
+          type='submit'
+          className='flex w-full items-center justify-center rounded-lg bg-red-500 hover:bg-red-600 px-4 py-2 text-sm font-semibold text-white sm:w-auto sm:px-8'
+        >
+          {formType === 'login' ? 'Login' : 'Register'}
+        </button>
+      </div>
+    </form>
+  );
 }
