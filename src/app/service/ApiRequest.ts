e28b29e9700abf272526aaed49fe4f5e36dcf5ac
Marcelo <marceloengecomp@gmail.com>
2023-11-28 16:17:36 -0300

    Api Request Service - implementation

--- src/app/service/ApiRequest.ts
+++ src/app/service/ApiRequest.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3333';
+const API_URL = process.env.API_URL || 'http://localhost:3333';
 
 export interface ApiQueryParams {
   [key: string]: string | number | boolean;
@@ -10,9 +10,17 @@ export interface RequestOptions {
   rating_like?: string;
 }
 
-export const defaultRequestOptions: RequestOptions = {
+export const defaultOptions: RequestOptions = {
   page: 1,
   _limit: 10,
+}
+
+export function buildQueryString(params: ApiQueryParams) {
+  const query = Object.entries(params)
+  .filter(([, value]) => value !== undefined)
+  .map(([key, value]) => [key, encodeURIComponent(String(value))]);
+
+  return `?${new URLSearchParams(Object.fromEntries(query)).toString()}`;
 }
 
 export async function apiRequest(
@@ -20,11 +28,17 @@ export async function apiRequest(
   query: ApiQueryParams = {}, 
   options: RequestOptions = {}
 ): Promise<any> {
+  const mergedOptions: RequestOptions = { ...defaultOptions, ...options };
+  const queryString: string = buildQueryString({ ...query, ...mergedOptions });
   try {
-    const response = fetch(`${API_URL}/${endpoint}`);
-    const data = await (await response).json();
-    return data;
+    const response = await fetch(`${API_URL}/${endpoint}${queryString}`);
+
+    if (!response.ok) {
+      throw new Error(`API request failed: ${response.statusText}`);
+    }
+
+    return response.json();
   } catch (error) {
-    console.log('Error: ', error);
+    throw error;
   }
 }
