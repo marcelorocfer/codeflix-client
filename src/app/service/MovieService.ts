575a175682989b814cf3682a93f262bd29c0424c
Marcelo <marceloengecomp@gmail.com>
2023-11-30 17:53:52 -0300

    Movie Service

---
+++ src/app/service/MovieService.ts
@@ -0,0 +1,36 @@
+import { Movie, Movies } from '../types/movie';
+import { RequestOptions, apiRequest } from './ApiRequest';
+
+export const getMovieById = async (id: string): Promise<Movie> => {
+  return await apiRequest(`movies/${encodeURIComponent(id)}`);
+}
+
+export const getFeatureMovies = async (id: string): Promise<Movie> => {
+  return await apiRequest(`featured/${id}`);
+}
+
+export const getMoviesByGenre = async (
+  genre: string, 
+  options?: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`, 
+    { genres_like: encodeURIComponent(genre) }, 
+    options
+  );
+}
+
+export const searchMovies = async (
+  title: string = '',
+  genre: string = '',
+  options?: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`, 
+    { 
+      title_like: encodeURIComponent(title),
+      genres_like: encodeURIComponent(genre)
+    }, 
+    options
+  );
+}
