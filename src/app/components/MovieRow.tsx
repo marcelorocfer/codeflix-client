d86c0dad872a6bdbbb7a43b9409a898ca794f146
Marcelo <marceloengecomp@gmail.com>
2023-12-01 18:43:22 -0300

    Movie Card Info

--- src/app/components/MovieRow.tsx
+++ src/app/components/MovieRow.tsx
@@ -1,26 +1,30 @@
 import Image from 'next/image';
-import { Movies } from '../types/movie';
+import { Movie, Movies } from '../types/movie';
 
 type MovieRowProps = {
   sectionTitle: string;
   movies: Movies
 };
 
-type MovieCardProps = {
-  index: number;
-};
-
-const MovieCard = ({ index }: MovieCardProps) => (
-  <div
-    key={index}
-    className='group relative h-28 min-w-[200px] cursor-pointer rounded bg-gradient-to-t from-transparent to-black 
-      transition-transform duration-200 ease-out hover:opacity-100 md:h-36 md:min-w-[260px] md:hover:scale-110'>
+const MovieCard = ({ movie }: { movie: Movie }) => (
+  <div className='group relative min-h-[12vh] rounded bg-zinc-900 md:min-h-[12vw]'>
     <Image
-      src={`/item_${index}.png`}
-      fill={true}
-      alt='MAID'
-      className='rounded' 
+      alt={movie.title}
+      src={movie.bannerFileURL}
+      width={600}
+      height={400}
+      className='rounded-md object-cover object-top transition'
     />
+
+    <div className='invisible absolute top-0 z-10 w-full min-w-[20vw] scale-0 opacity-0 transition delay-300 duration-200 group-hover:-translate-y-[6vw] group-hover:scale-110 group-hover:opacity-100 sm:visible'>
+      <Image
+        src={movie.bannerFileURL}
+        alt={movie.title}
+        width={600}
+        height={400}
+        className='duration h-[12vw] w-full cursor-pointer rounded-t-md object-cover object-top shadow-xl transition'
+      />
+    </div>
   </div>
 );
 
@@ -32,9 +36,9 @@ export function MovieRow({ sectionTitle, movies }: MovieRowProps) {
           {sectionTitle}
         </h2>
       </div>
-      <div className='-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide'>
+      <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8'>
         {movies.map((movie) => (
-          <h1 key={movie.id}>{movie.title}</h1>
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
