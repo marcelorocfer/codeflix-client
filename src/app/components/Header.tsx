9e1a41d0e729236d366a7d23ae3260094e1ddb59
Marcelo <marceloengecomp@gmail.com>
2023-12-12 17:44:21 -0300

    Header adjustment

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,6 +6,7 @@ import { NavLinks } from './NavLinks';
 import { useScroll } from '../hooks/useScroll';
 import { SearchForm } from './SearchForm';
 import { useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Header() {
   const isScrolled = useScroll();
@@ -28,21 +29,23 @@ export default function Header() {
   return (
     <header
       className={`${isScrolled && 'bg-black'}
-      fixed top-0 z-50 
+      fixed top-0 z-50
       flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <Logo />
+        <Link href='/'>
+          <Logo />
+        </Link>
         <NavLinks />
       </div>
-      <div className='flex items-center space-x-2 md:space-x-8'>      
-        <SearchForm 
+      <div className='flex items-center space-x-2 md:space-x-8'>
+        <SearchForm
+          onSearch={onSearch}
           searchTerm={searchTerm}
           onSearchTermChange={onSearchTermChange}
-          onSearch={onSearch}
         />
+        <UserProfile />
       </div>
-      <UserProfile />
     </header>
   );
 }
